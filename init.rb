085e925b2fa28251a5b518d3e6ec73d16f58f9bc
Vincent Robert <vincent.robert@nanego.com>
2017-02-13 13:07:59 +0100

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_periodic_jobs do
+  name 'Redmine Periodic Jobs plugin'
+  author 'Vincent ROBERT'
+  description 'This is a plugin for Redmine'
+  version '0.1.0'
+  url 'https://github.com/nanego/redmine_periodic_jobs'
+  author_url 'mailto:contact@vincent-robert.com'
+end
